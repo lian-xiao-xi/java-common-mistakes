7bb293434c95933399aa79b004a087eef753a049
BAOZUN\jm014326 <ning.li@baozun.com>
2021-01-19 16:16:51 +0800

    feat: SpringBoot 项目初始化

---
+++ src/main/java/org/geekbang/time/commonmistakes/JavaCommonMistakesApplication.java
@@ -0,0 +1,13 @@
+package org.geekbang.time.commonmistakes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCommonMistakesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCommonMistakesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/geekbang/time/commonmistakes/JavaCommonMistakesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.geekbang.time.commonmistakes;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCommonMistakesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
