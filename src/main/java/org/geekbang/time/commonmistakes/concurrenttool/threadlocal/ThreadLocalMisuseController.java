addad59be0cbfaccc3936981ceb35b24b5b80fcf
BAOZUN\jm014326 <ning.li@baozun.com>
2021-01-19 18:45:32 +0800

    doc: add TODO

--- src/main/java/org/geekbang/time/commonmistakes/concurrenttool/threadlocal/ThreadLocalMisuseController.java
+++ src/main/java/org/geekbang/time/commonmistakes/concurrenttool/threadlocal/ThreadLocalMisuseController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
 import java.util.Map;
 
+/*
+* TODO ThreadLocal 相关的资料链接
+* */
 @RestController
 @RequestMapping("/threadlocal")
 public class ThreadLocalMisuseController {
